54b325b02501150a5cf130b94b5efbf78578c437
hahoyer <hahoyer@gmail.com>
2026-02-28 22:54:17 +0100

    change: Very simple things

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GlobalVariables : MonoBehaviour
 {
-    //Array of GameObjects for Dishes
+    [FormerlySerializedAs("_dishes")] [SerializeField]
+    public static GameObject[] Dishes;
     //Click Damage int variable
     //Score int variable
 }
--- Assets/Scripts/PawScript.cs
+++ Assets/Scripts/PawScript.cs
@@ -1,26 +1,77 @@
 using UnityEngine;
 
-//Add a serialized field for "Target Food" GameObject
-//Serialized Field for "Speed" float
-//Serialized Field for "Health" int
-//Serialized Field for "Reached" bool
+// ReSharper disable Unity.PerformanceCriticalCodeInvocation
 
 public class PawScript : MonoBehaviour
 {
-    void Start()
-    {
-        //Pick a random Dish and set it to Target Food
-        //Should spawn at a random point around the edge of the table, but should be close to the target food
-    }
+    [SerializeField] GameObject dishPrefab;
+    [SerializeField] GameObject tableObject;
+    [SerializeField] float speed;
+    [SerializeField] int health;
+    GameObject _dishObject;
+    Camera _camera;
+
+    void Awake() => _camera = Camera.main;
 
     void Update()
     {
-        //Check if "Reached" is true, if true then add the food in scene as a child of the paw + return to original position
+        if (IsHiding)
+        {
+            transform.position += new Vector3(-0.1f, 0, 0);
+            if (IsOverlappingWithTable)
+                return;
 
-        //Move towards the food based on the speed
-        //When reaching dish, set "Reached" to true
+            _dishObject = SpawnDishOnTable();
+            return;
+        }
 
-        //On click, should reduce health by 1 (for now, will need to add a global variable for the click damage)
-        //after the click, check for health, and if <1 then destroy self
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsOverlappingWithMouse)
+                return;
+
+            health--;
+            if (health > 0)
+                return;
+            Destroy(this);
+            return;
+        }
+
+        if (IsOverlappingWithDish)
+        {
+            _dishObject.transform.SetParent(transform);
+            _dishObject.SetActive(false);
+            _dishObject = null;
+
+            return;
+        }
+
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _dishObject.transform.position, step);
+    }
+
+    bool IsOverlappingWithMouse =>
+        GetComponent<Collider2D>().OverlapPoint(_camera.ScreenToWorldPoint(Input.mousePosition));
+
+    bool IsOverlappingWithDish => _dishObject.GetComponent<Collider2D>().IsTouching(GetComponent<Collider2D>());
+    bool IsOverlappingWithTable => GetComponent<Collider2D>().IsTouching(tableObject.GetComponent<Collider2D>());
+    bool IsHiding => _dishObject == null;
+
+    GameObject SpawnDishOnTable()
+    {
+        var tableRenderer = tableObject.GetComponent<SpriteRenderer>();
+        var dishRenderer = dishPrefab.GetComponent<SpriteRenderer>();
+
+        var tableBounds = tableRenderer.bounds;
+        var dishExtents = dishRenderer.bounds.extents * 0.8f;
+
+        var spawnPosition = new Vector3
+        (
+            Random.Range(tableBounds.min.x + dishExtents.x, tableBounds.max.x - dishExtents.x),
+            Random.Range(tableBounds.min.y + dishExtents.y, tableBounds.max.y - dishExtents.y),
+            tableBounds.center.z
+        );
+
+        return Instantiate(dishPrefab, spawnPosition, Quaternion.identity);
     }
 }
